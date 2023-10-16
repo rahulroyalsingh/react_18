a7e32049c245b31d47b1cd3013bbb119c9c042f4
rahul singh <116128750+rahulroyalsingh@users.noreply.github.com>
2023-10-16 11:59:50 +0530

    validation in user and password both

--- Login.js
+++ Login.js
@@ -3,6 +3,7 @@ function Login() {
     const [user, setUser] = useState('');
     const [password, setPassword]=useState('');
     const [userErr, setUserErr]=useState(false);
+    const [passErr, setPassErr]=useState(false);
     function loginHandle(e) 
     {
         e.preventDefault()
@@ -18,12 +19,23 @@ function Login() {
         setUserErr(false)
        }
     }
+    function passwordHandler(e){
+        let item=e.target.value;
+        if(item.length<4 )
+        {
+            setPassErr(true)
+        }
+        else
+        {
+            setPassErr(false)
+        }
+    }
     return (
         <div>
             <h1>Login</h1>
             <form onSubmit={loginHandle}>
                 <input type="type here" placeholder="User Id" onChange={userHandler}/>{userErr?<span>user not valid</span>:''}  <br /> <br />
-                <input type="type here" placeholder="Password" /><br /> <br />
+                <input type="type here" placeholder="Enter user Password" onChange={passwordHandler} />{passErr?<span>Password Not Valid</span>:''}<br /> <br />
                 <button type='Submit'>login</button>
             </form>
         </div>
